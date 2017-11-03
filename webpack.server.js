93d49a5b84867d9daafb355cbcd92f55604fd40d
rayGreycloud <rayGreycloud@gmail.com>
2017-11-03 15:43:39 -0700

    Add webpack configuration

---
+++ webpack.server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+modules.exports = {
+  // Set build for node instead of browser
+  target: 'node',
+  // App root file 
+  entry: './src/index.js',
+  // Output file location
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  // Babel 
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react', 
+            'stage-0',
+            [
+              'env', 
+              { targets: { 
+                browers: ['last 2 versions'] 
+              }}
+            ]
+          ]
+        }
+      }
+    ]
+  }
+};
