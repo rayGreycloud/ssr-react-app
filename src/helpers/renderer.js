b9ff484511c5b07081a2edd8819284778641ee0f
rayGreycloud <rayGreycloud@gmail.com>
2017-11-08 15:57:23 -0800

    Implement react-router-config and update route uses

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { renderRoutes } from 'react-router-config';
+ 
 import Routes from '../client/Routes';
 
 export default (req, store) => {
   const content = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.path} context={{}}>
-        <Routes />
+        <div>{renderRoutes(Routes)}</div>
       </StaticRouter>      
     </Provider>    
   );
   
   return `
     <html>
-      <head></head>
+      <head><title>SSR</title></head>
       <body>
         <div id="root">${content}</div>
         <script src="bundle.js"></script>
