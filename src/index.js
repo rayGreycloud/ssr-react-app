ee4314cd10da81c8c3661afcd216e2428d2ed9bf
rayGreycloud <rayGreycloud@gmail.com>
2017-11-08 09:48:25 -0800

    Create server store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,11 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+  
+  // Initialize and load data into store 
+  
+  res.send(renderer(req, store));
 });
 
 app.listen(port, () => {
