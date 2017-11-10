e58661d092710b19bc352618ead5855d5ba36046
rayGreycloud <rayGreycloud@gmail.com>
2017-11-10 07:36:11 -0800

    Set initial state to server store and pass in state to client store

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -20,6 +20,9 @@ export default (req, store) => {
       <head><title>SSR</title></head>
       <body>
         <div id="root">${content}</div>
+        <script>
+          window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+        </script>
         <script src="bundle.js"></script>
       </body>
     </html>
