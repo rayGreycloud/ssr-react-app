2f4c31a15aeba951e50ddfea7527025f422e03f5
rayGreycloud <rayGreycloud@gmail.com>
2017-11-11 11:59:51 -0800

    Setup axios instance for server

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const port = 3000;
 
 app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
   proxyReqOptDecorator(opts) {
-    opts.header['x-forwarded-host'] = 'localhost:3000';
+    opts.headers['x-forwarded-host'] = 'localhost:3000';
     return opts;
   }
 }));
@@ -19,7 +19,7 @@ app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  const store = createStore();
+  const store = createStore(req);
   
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
